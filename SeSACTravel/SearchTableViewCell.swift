4da923bd6d573561176ca8336472f246f639ebf5
Maekiee <pdwssf@gmail.com>
2025-07-16 02:32:28 +0900

    카테고리 추가

--- SeSACTravel/SearchTableViewCell.swift
+++ SeSACTravel/SearchTableViewCell.swift
@@ -1,17 +1,36 @@
-
 import UIKit
+import Kingfisher
 
 class SearchTableViewCell: UITableViewCell {
 
+    @IBOutlet var thumbnailImageview: UIImageView!
+    @IBOutlet var cityTagLabel: UILabel!
+    @IBOutlet var cityTitleLabel: [UILabel]!
+    @IBOutlet var verticalDivider: UIView!
+    @IBOutlet var cityTagContainer: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        verticalDivider.backgroundColor = .white
+        thumbnailImageview.contentMode = .scaleAspectFill
+        cityTagContainer.backgroundColor = .black.withAlphaComponent(0.5)
+        cityTagLabel.textColor = .lightGray
+        cityTagLabel.textColor = .white
+
+        thumbnailImageview.backgroundColor = .lightGray
+        for i in cityTitleLabel {
+            i.textColor = .white
+            i.font = .boldSystemFont(ofSize: 20)
+        }
         
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    
+    func configureCellUI(rowData: City) {
+        let thumbnailUrl = URL(string: rowData.city_image)
         
+        thumbnailImageview.kf.setImage(with: thumbnailUrl)
+        cityTitleLabel[0].text = rowData.city_name
+        cityTitleLabel[1].text = rowData.city_english_name
+        cityTagLabel.text = rowData.city_explain
     }
     
 }
--- SeSACTravel/SearchTableViewController.swift
+++ SeSACTravel/SearchTableViewController.swift
@@ -1,22 +1,59 @@
 import UIKit
 
-class SearchTableViewController: UITableViewController {
 
+enum CityType: String, CaseIterable {
+    case all = "모두"
+    case domestic = "국내"
+    case international = "해외"
+}
+
+class SearchTableViewController: UITableViewController {
+    let cityInfo = CityInfo()
+    var displayOnCitys: [City] = []
+    var currentCategory: CityType = .all
+    static let identifier: String = "SearchTableViewCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "인기 도시"
+        let xib = UINib(nibName: SearchTableViewController.identifier, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: SearchTableViewController.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
     }
-
-
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
+        return displayOnCitys.isEmpty ? cityInfo.city.count : displayOnCitys.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewController.identifier, for: indexPath) as! SearchTableViewCell
+        
+        cell.configureCellUI(rowData: displayOnCitys.isEmpty ? cityInfo.city[indexPath.row]: displayOnCitys[indexPath.row] )
+        
+        return cell
+    }
+    
+    
 
+    @IBAction func segCategory(_ sender: UISegmentedControl) {
+        print(#function, sender.selectedSegmentIndex)
+        let index = sender.selectedSegmentIndex
+        currentCategory = CityType.allCases[index]
+        
+        switch currentCategory {
+        case CityType.all:
+            displayOnCitys = cityInfo.city
+            tableView.reloadData()
+        case CityType.domestic:
+            displayOnCitys = cityInfo.city.filter { $0.domestic_travel == true }
+            tableView.reloadData()
+        case CityType.international:
+            displayOnCitys = cityInfo.city.filter { $0.domestic_travel == false }
+            tableView.reloadData()
+        default:
+            displayOnCitys = cityInfo.city
+            break
+        }
+    }
 }
