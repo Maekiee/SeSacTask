5638a8a11de13a0afcf0375b82d85b0fc2fd8695
Maekiee <pdwssf@gmail.com>
2025-07-15 17:30:15 +0900

    search tableViewController 추가 SearchTableViewCell 추가 SearchTableView xib 추가

---
+++ SeSACTravel/SearchTableViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "인기 도시"
+    }
+
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+}
